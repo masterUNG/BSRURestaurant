ee226175e62311b3485d6fe4a6769c9368ae419b
masterUNG <phrombutr@gmail.com>
2015-02-14 15:55:02 +0700

    Delete All Data

--- app/src/main/java/appewtc/masterung/bsrurestaurant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/bsrurestaurant/MainActivity.java
@@ -1,9 +1,25 @@
 package appewtc.masterung.bsrurestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,11 +37,98 @@ public class MainActivity extends ActionBarActivity {
         objOrderTABLE = new OrderTABLE(this);
 
         //Test Add Value
-        objUserTABLE.addValeToUser("User", "Password", "Name");
-        objOrderTABLE.addValuetoOrder("Officer", "Drink", "Food", "Desk");
+//        objUserTABLE.addValeToUser("User", "Password", "Name");
+//        objOrderTABLE.addValuetoOrder("Officer", "Drink", "Food", "Desk");
+
+        //Delete All Data
+        deleteAllData();
+        
+        //Synchronize JSON to SQLite
+        synJSONtoSQLite();
 
 
     }   // onCreate
+
+    private void deleteAllData() {
+
+        SQLiteDatabase objSQLite = openOrCreateDatabase("bsru.db", MODE_PRIVATE, null);
+        objSQLite.delete("userTABLE", null, null);
+
+    }   //delete All Data
+
+    private void synJSONtoSQLite() {
+
+        // Open Permit All
+        if (Build.VERSION.SDK_INT > 9) {
+
+            StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(myPolicy);
+
+        }   // if
+
+
+        InputStream objInputStream = null;
+        String strJSON = "";
+
+        //Create InputStream
+        try {
+
+            HttpClient objHttpClient = new DefaultHttpClient();
+            HttpPost objHttpPost = new HttpPost("http://swiftcodingthai.com/bsru/php_get_data_master.php");
+            HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+            HttpEntity objHttpEntity = objHttpResponse.getEntity();
+            objInputStream = objHttpEntity.getContent();
+
+        } catch (Exception e) {
+            Log.d("bsru", "InputStream ==> " + e.toString());
+        }
+
+
+        //Create strJSON
+        try {
+
+            BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+            StringBuilder objStringBuilder = new StringBuilder();
+            String strLine = null;
+
+            while ((strLine = objBufferedReader.readLine()) != null ) {
+
+                objStringBuilder.append(strLine);
+
+            }   //while
+
+            objInputStream.close();
+            strJSON = objStringBuilder.toString();
+
+
+        } catch (Exception e) {
+            Log.d("bsru", "strJSON ==> " + e.toString());
+        }
+
+
+        //UpDate Value to SQLite
+        try {
+
+            final JSONArray objJSONArray = new JSONArray(strJSON);
+
+            for (int i = 0; i < objJSONArray.length(); i++) {
+
+                JSONObject objJSONObject = objJSONArray.getJSONObject(i);
+                String strUser = objJSONObject.getString("User");
+                String strPassword = objJSONObject.getString("Password");
+                String strName = objJSONObject.getString("Name");
+
+                long insertData = objUserTABLE.addValeToUser(strUser, strPassword, strName);
+
+            }   // for
+
+        } catch (Exception e) {
+            Log.d("bsru", "Updata ==> " + e.toString());
+        }
+
+
+
+    }   //synJSONtoSQLite
 
 
     @Override
